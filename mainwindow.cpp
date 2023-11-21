9048d323be1864bf15c17db2819a0fd19bd5fe0b
yang1556 <92725391+yang1556@users.noreply.github.com>
2023-11-21 18:41:57 +0800

    新增main

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+
+}
+
